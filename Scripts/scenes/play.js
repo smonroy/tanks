32a9778e572ad28621f53d711077cde001235e95
Wesley Angus <wesman93@gmail.com>
2018-10-20 14:29:23 -0400

    Fixed js files from merge

--- Scripts/scenes/play.js
+++ Scripts/scenes/play.js
@@ -19,6 +19,10 @@ var scenes;
         Update() {
             this._map.tank1.Update();
             this._map.tank2.Update();
+            if (managers.Collision.isColliding(this._map.tank1, this._map.tank2)) {
+                this._map.tank1.Reset();
+                this._map.tank2.Reset();
+            }
             if (managers.Game.scoreBoard.isBattleOver()) {
                 if (this._level == 3) {
                     managers.Game.currentState = config.Scene.START;
